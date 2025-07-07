cc62533843e70f6cf3a83caee2e6cc751aa40890
Veinix <davidaviles_dev@outlook.com>
2025-07-07 13:13:30 +0300

    save

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    root: "app",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "app"),
