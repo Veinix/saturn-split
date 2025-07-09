d93e50471e8b64ea9a566a9b34a3460230318c30
David Aviles <17769104+Veinix@users.noreply.github.com>
2025-07-09 15:48:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
         tsconfigPaths(),
         cloudflare(),
     ],
+base: "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "app"),
