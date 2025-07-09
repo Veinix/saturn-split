fe31fbe647ecc410d10760e4470403c248c98d0f
Veinix <davidaviles_dev@outlook.com>
2025-07-09 12:46:09 +0300

    save

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
     ssr: false, // SPA Mode
+    appDirectory: "app",
+    routeDiscovery: { mode: "initial" }
 } satisfies Config;
- 
